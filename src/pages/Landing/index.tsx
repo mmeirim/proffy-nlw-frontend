1ac53b82854daaf08096bcdc8d919ad89ed3ab55
mmeirim <mmeirim@outlook.com>
2020-08-07 22:36:39 -0300

    Finaliza css e cria integração da api com teacher form

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom'
 
 import LogoImg from '../../assets/images/logo.svg'
@@ -8,8 +8,18 @@ import GiveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import PurpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 
 import './styles.css';
+import api from '../../services/api';
 
 function Landing(){
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(()=> {
+        api.get('connections').then(response => {
+            const {total} = response.data;
+            setTotalConnections(total);
+        })
+    }, []);
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -36,7 +46,7 @@ function Landing(){
                 </div>
                 
                 <span className="total-conections">
-                    Total de 200 conexões já realizadas <img src={PurpleHeartIcon} alt="Coração roxo"/>
+                    Total de {totalConnections} conexões já realizadas <img src={PurpleHeartIcon} alt="Coração roxo"/>
                 </span>
             </div>
         </div>
